c40ff9a2fb949a0b42f31a6800998dcda7a399d0
sarahbruh <sarah.brunmayr@htl-wels.com>
2021-09-30 15:12:10 +0200

    main added

--- src/CountPrinter.java
+++ src/CountPrinter.java
@@ -5,10 +5,5 @@
  */
 
 public class CountPrinter implements Runnable{
-    @Override
-    public void run(){
-        for (int i=0; i<=20; i++){
-            System.out.println(i);
-        }
-    }
+
 }
