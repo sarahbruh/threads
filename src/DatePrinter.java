9050411c6b09aee7254790d3ae88897e258e63b7
sarahbruh <sarah.brunmayr@htl-wels.com>
2021-09-30 15:13:11 +0200

    DatePrinter added

--- src/DatePrinter.java
+++ src/DatePrinter.java
@@ -7,5 +7,12 @@ import java.util.Date;
  */
 
 public class DatePrinter implements Runnable{
+    Date date = new Date();
 
+    @Override
+    public void run(){
+        for (int i=0; i<=20; i++){
+            System.out.println(date);
+        }
+    }
 }
